68e5061723b3abf9b35f812ca1c6b948f5d4dfc0
omagdiel <omagdiel@gmail.com>
2018-02-08 07:45:06 -0700

    Added new parameters for user registration and editing account.

--- db/migrate/20180208140739_devise_create_users.rb
+++ db/migrate/20180208140739_devise_create_users.rb
@@ -7,6 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ##custom field
+      t.string :first_name
+      t.string :last_name
+
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
